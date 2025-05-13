23c96b49dfc87673d1711d2729a0904b10603a6a
Jess Frazelle <github@jessfraz.com>
2025-05-12 19:11:13 -0700

    move basic revolve circle
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- rust/kcl-lib/src/simulation_tests.rs
+++ rust/kcl-lib/src/simulation_tests.rs
@@ -2962,3 +2962,24 @@ mod subtract_regression05 {
         super::execute(TEST_NAME, true).await
     }
 }
+mod basic_revolve_circle {
+    const TEST_NAME: &str = "basic_revolve_circle";
+
+    /// Test parsing KCL.
+    #[test]
+    fn parse() {
+        super::parse(TEST_NAME)
+    }
+
+    /// Test that parsing and unparsing KCL produces the original KCL input.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn unparse() {
+        super::unparse(TEST_NAME).await
+    }
+
+    /// Test that KCL is executed correctly.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn kcl_test_execute() {
+        super::execute(TEST_NAME, true).await
+    }
+}
